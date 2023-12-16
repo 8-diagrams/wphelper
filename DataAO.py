55bf1be38062c0271d57a54485743367626d457e
8-diagrams <55012521+8-diagrams@users.noreply.github.com>
2023-12-16 14:13:25 +0800

    Update DataAO.py

--- DataAO.py
+++ DataAO.py
@@ -15,7 +15,7 @@ def access(tg_id):
     users_cnt = row[0]
     level = 0 if users_cnt > 0 else 99 
     ins_sql = 'insert into admin (tg_id, role) values (%s, %s) on update key update access_time =  access_time+1  '
-    cur.excute(ins_sql , [ tg_id  ,level] )
+    cur.execute(ins_sql , [ tg_id  ,level] )
     conn.commit()
     cur.close()
 
